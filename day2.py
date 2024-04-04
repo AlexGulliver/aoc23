25a80d567cf8f136165d41d178b38ffd8600a97b
Alex Gulliver <alexgulliver@Alexs-MacBook-Pro.local>
2024-04-04 21:47:15 +0100

    Week 2 part 2

--- day2.py
+++ day2.py
@@ -8,7 +8,7 @@ input = open(inputfile, "r")
 
 gamenumber = 0
 gametotal = 0
-
+powertotal = 0
 # Splits up lines in the text file
 for line in input:
     # Start variable is index of ':'
@@ -18,22 +18,43 @@ for line in input:
     # Divides each game into their representative handouts
     handouts = game.split(";")
     validgame = True
+    reds = []
+    blues = []
+    greens = []
     for hand in handouts:
         colours = hand.split(',')
         for y in colours:
             v = y.lstrip()
             if 'red' in v:
+                reds.append(int(v[:2]))
                 if int(v[:2]) > max_red:
                     validgame = False
             if 'green' in v:
+                greens.append(int(v[:2]))
                 if int(v[:2]) > max_green:
                     validgame = False
             if 'blue' in v:
+                blues.append(int(v[:2]))
                 if int(v[:2]) > max_blue:
                     validgame = False
+    reds.sort()
+    greens.sort()
+    blues.sort()
+    print(reds)
+    print(greens)
+    print(blues)
+    power = reds[-1] * greens[-1] * blues[-1]
+    print(power)
+    powertotal += power
+    print("POWERTOTAL", powertotal)
+    print("end of game")
     gamenumber = gamenumber + 1
     if validgame == True:
         gametotal = gametotal + gamenumber
     #print("Game number is", gamenumber)
-    
+
+# Part 1 solution
 print("Total is" ,gametotal) 
+
+#Part 2 solution
+print("Power total = ", powertotal)
